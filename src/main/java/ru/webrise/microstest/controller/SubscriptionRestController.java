179271b50ffdc9d103d2d30a0447568c4af1f112
OlegZabolotnykh <oleg.zabolotnykh@gmail.com>
2025-05-22 12:46:45 +0300

    Added logging slf4j(logback). Controller classes are rearranged a little.

--- src/main/java/ru/webrise/microstest/controller/SubscriptionRestController.java
+++ src/main/java/ru/webrise/microstest/controller/SubscriptionRestController.java
@@ -1,5 +1,6 @@
-package ru.webrise.microstest.controller.rest;
+package ru.webrise.microstest.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.webrise.microstest.dto.SubscriptionDto;
 import ru.webrise.microstest.service.SubscriptionService;
@@ -8,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/subscriptions")
+@Slf4j
 public class SubscriptionRestController {
 
     private final SubscriptionService subscriptionService;
@@ -18,26 +20,31 @@ public class SubscriptionRestController {
 
     @GetMapping
     public List<SubscriptionDto> getAllSubscriptions() {
+        log.debug("Get all subscriptions");
         return subscriptionService.getAllSubscriptions();
     }
 
     @GetMapping("{id}")
     public SubscriptionDto getSubscription(@PathVariable Long id) {
+        log.debug("Get subscription by id={}", id);
         return subscriptionService.getSubscriptionById(id);
     }
 
     @PostMapping
     public SubscriptionDto addNewSubscription(@RequestBody SubscriptionDto subscriptionDto) {
+        log.debug("Add new subscription subscriptionDto={}", subscriptionDto);
         return subscriptionService.addNewSubscription(subscriptionDto);
     }
 
     @DeleteMapping("{id}")
     public void deleteSubscription(@PathVariable Long id) {
+        log.debug("Delete subscription by id={}", id);
         subscriptionService.deleteSubscription(id);
     }
 
     @GetMapping("top")
     public List<SubscriptionDto> getTop3Popular() {
+        log.debug("Get top-3 subscriptions");
         return subscriptionService.getTop3Popular();
     }
 }
--- src/main/java/ru/webrise/microstest/controller/UserRestController.java
+++ src/main/java/ru/webrise/microstest/controller/UserRestController.java
@@ -1,62 +1,66 @@
-package ru.webrise.microstest.controller.rest;
+package ru.webrise.microstest.controller;
 
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.webrise.microstest.dto.UserDto;
 import ru.webrise.microstest.entity.Subscription;
 import ru.webrise.microstest.service.UserServiceImpl;
-import ru.webrise.microstest.util.UserMapper;
 
 import java.util.List;
 import java.util.Set;
 
 @RestController
 @RequestMapping("api/v1/users")
+@Slf4j
 public class UserRestController {
     private final UserServiceImpl userService;
-    private final UserMapper userMapper;
 
-    public UserRestController(UserServiceImpl userService, UserMapper userMapper) {
+    public UserRestController(UserServiceImpl userService) {
         this.userService = userService;
-        this.userMapper = userMapper;
     }
 
     @GetMapping
     public List<UserDto> getAllUsers() {
+        log.debug("Get all users");
         return userService.getAllUsers();
     }
 
     @GetMapping("{id}")
     public UserDto getUserById(@PathVariable Long id) {
+        log.debug("Get user by id={}", id);
         return userService.getUserById(id);
     }
 
     @PostMapping
     public UserDto addUser(@RequestBody UserDto userDto) {
+        log.debug("Add user. userDto={}", userDto);
         return userService.addUser(userDto);
     }
 
     @PutMapping("{id}")
     @Transactional
     public UserDto updateUser(@PathVariable Long id,
-                           @RequestBody UserDto userDto) {
-
+                              @RequestBody UserDto userDto) {
+        log.debug("Update user. id={}, userDto={}", id, userDto);
         return userService.updateUser(id, userDto);
     }
 
     @DeleteMapping("{id}")
     public void deleteUser(@PathVariable Long id) {
+        log.debug("Delete user by id. id={}", id);
         userService.deleteUser(id);
     }
 
     @GetMapping("{id}/subscriptions")
     public Set<Subscription> getUserSubscriptions(@PathVariable Long id) {
+        log.debug("Get user subscriptions. User id={}", id);
         return userService.getUserSubscriptions(id);
     }
 
     @DeleteMapping("{id}/subscriptions/{sub_id}")
     public void deleteUserSubscription(@PathVariable Long id, @PathVariable Long sub_id) {
+        log.debug("Delete user subscription. User id={}, Subscription sub_id={}", id, sub_id);
         userService.deleteSubscription(id, sub_id);
     }
-
 }
